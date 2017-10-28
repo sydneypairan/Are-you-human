a4a51b1e64cefaea115596526321f5156345bb96
sydneypairan <33166540+sydneypairan@users.noreply.github.com>
2017-10-27 20:39:46 -0400

    added text

---
+++ larry/larry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace larry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Is lary the best cat in the world?");
+
+            Console.Write("You said " + Console.ReadLine());
+
+            Console.ReadLine(); //ReadLine prevents the console from closing until it gets a response
+            Console.Write("you're wrong you must be a robot");
+            Console.ReadLine();
+        }
+    }
+}
+    
