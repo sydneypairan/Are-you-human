3bd4a5cee65c5119598dce7380ec4685bd7d59ef
Sydney <sydneypairan88@gmail.com>
2017-12-28 11:57:12 -0500

    added 5 facts and fixed spacing issues

--- larry/larry/Program.cs
+++ larry/larry/Program.cs
@@ -10,14 +10,69 @@ namespace larry
     {
         static void Main(string[] args)
         {
-            Console.Write("Is lary the best cat in the world?");
+            Console.WriteLine("Is lary the best cat in the world?[True/False]");
 
-            Console.Write("You said " + Console.ReadLine());
+            bool response = Convert.ToBoolean(Console.ReadLine());
 
-            Console.ReadLine(); //ReadLine prevents the console from closing until it gets a response
-            Console.Write("you're wrong you must be a robot");
-            Console.ReadLine();
+            string Fact1 = "He is white.";
+            string Fact2 = "He is fluffy.";
+            string Fact3 = "He enjoys eating";
+            string Fact4 = "He is a boy.";
+            string Fact5 = "He likes ice cream.";
+
+            if (response == false)
+            { //ReadLine prevents the console from closing until it gets a response
+
+
+                Console.BackgroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine("you're wrong you must be a robot");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+
+            else
+            {
+                Console.WriteLine("You said " + response);
+
+
+            }
+            Console.WriteLine(" What fact do you want to know about Larry? [1-5]");
+
+
+            string answer = Console.ReadLine();
+            if (answer == "1")
+
+            { Console.WriteLine(Fact1);}
+
+           else if (answer == "2")
+
+            { Console.WriteLine(Fact2);}
+
+           else if (answer=="3")
+
+            { Console.WriteLine(Fact3); }
+
+          else  if (answer=="4")
+            { Console.WriteLine(Fact4); }
+
+          else  if (answer == "5")
+            { Console.WriteLine(Fact5); }
+
+            else { Console.WriteLine("must be value between 1 and 5"); }
+
+            Console.WriteLine("what other facts would you like to know? 6-10");
+            string answer2=Console.ReadLine();
+
+            switch (answer2)
+            {
+                case "6": Console.WriteLine("fact 6 goes here");
+                    break;
+                default:
+                    Console.WriteLine("please enter number between 6 and 10");
+                    break;
+            }
+                
+                Console.ReadLine();
         }
     }
 }
-    
